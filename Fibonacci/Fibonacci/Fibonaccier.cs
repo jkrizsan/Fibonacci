8cedb3f56ee32f20aad0c0f56995b80bf6fa2cd8
József Krizsán <fysikko@gmail.com>
2022-01-16 09:17:11 +0100

    UI

---
+++ Fibonacci/Fibonacci/Fibonaccier.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    internal class Fibonaccier
+    {
+        /// <summary>
+        /// Entry point
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            writeToConsole($"{nameof(Fibonaccier)} application is Started");
+
+            do
+            {
+                writeToConsole("Please give me a positive number:");
+
+                callLogic(readFromConsole());
+
+                writeToConsole("Do you wouldl like to Continue? [Y]/[N]");
+
+            } while (readFromConsole().ToUpperInvariant() == "Y");
+
+            writeToConsole($"{nameof(Fibonaccier)} application is Ended");
+        }
+
+        static void callLogic(string input)
+        {
+            writeToConsole(input);
+        }
+
+        static void writeToConsole(string message)
+            => Console.WriteLine($"{message}{Environment.NewLine}");
+
+        static string readFromConsole()
+            => Console.ReadLine();
+
+    }
+}
